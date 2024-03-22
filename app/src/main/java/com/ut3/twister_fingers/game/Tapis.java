169769140caef905bef2923075509caef2818479
BBtocchi <bertocchileadu32@gmail.com>
2024-03-22 10:26:28 +0100

    Add tapis

--- app/src/main/java/com/ut3/twister_fingers/game/Tapis.java
+++ app/src/main/java/com/ut3/twister_fingers/game/Tapis.java
@@ -1,8 +1,12 @@
 package com.ut3.twister_fingers.game;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 
@@ -11,9 +15,19 @@ public class Tapis extends SurfaceView {
     ArrayList<Circle> circles = new ArrayList<>();
     public Tapis(Context context) {
         super(context);
-        circles.add(new Circle(context,100,100,100));
-        circles.add(new Circle(context,300,200,100));
-        circles.add(new Circle(context,600,600,100));
+        int width= Resources.getSystem().getDisplayMetrics().widthPixels;
+        int height=Resources.getSystem().getDisplayMetrics().heightPixels;
+
+        for(int i=1; i<=4;i++) {
+            for(int j=1; j<=4;j++){
+                circles.add(new Circle(context,
+                        ((width / 4) * i)-100,
+                        ((height/ 4) * j)-100 ,
+                        100)
+                );
+            }
+
+        }
 
     }
 
